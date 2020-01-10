38d9729895ddc1a4c38162b0e3a62ce63a491b80
Zane Paton <zanerepaton@gmail.com>
2020-01-10 13:14:28 -0600

    fix file link

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var markers = [];
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
-    navigator.serviceWorker.register("sw.js").then(
+    navigator.serviceWorker.register("./sw.js").then(
       function(registration) {
         console.log(
           "ServiceWorker registration successful with scope: ",
