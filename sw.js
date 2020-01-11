59bf893612e3453c347eff5814b35c57f25b28ed
Zane Paton <zanerepaton@gmail.com>
2020-01-10 19:50:13 -0600

    fix file links

--- sw.js
+++ sw.js
@@ -2,22 +2,23 @@ const CACHE_NAME = "my-site-cache-v1";
 const urlsToCache = [
   "./index.html",
   "./restaurant.html",
-  "./dbhelper.js",
-  "./restaurantinfo.js",
+  "./js/dbhelper.js",
+  "./js/restaurantinfo.js",
   "./README.md",
-  "./main.js",
-  "./1.jpg",
-  "./2.jpg",
-  "./3.jpg",
-  "./4.jpg",
-  "./5.jpg",
-  "./6.jpg",
-  "./7.jpg",
-  "./8.jpg",
-  "./9.jpg",
-  "./10.jpg",
-  "./styles.css",
-  "./restaurants.json"
+  "./js/main.js",
+  "./img/1.jpg",
+  "./img/2.jpg",
+  "./img/3.jpg",
+  "./img/4.jpg",
+  "./img/5.jpg",
+  "./img/6.jpg",
+  "./img/7.jpg",
+  "./img/8.jpg",
+  "./img/9.jpg",
+  "./img/10.jpg",
+  "./css/styles.css",
+  "./data/restaurants.json",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", function(event) {
